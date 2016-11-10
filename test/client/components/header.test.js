c3c2f0bca9ff2e6588ac4e5da5dcf5ace98d5d61
Rob Pataki <rob@handcrafteddigital.london>
2016-11-10 18:06:55 +0000

    Update the header component tests

--- test/client/components/header.test.js
+++ test/client/components/header.test.js
@@ -4,30 +4,42 @@ import {shallow} from 'enzyme';
 
 import Header from '../../../code/client/components/header';
 
+const { describe, it, beforeEach } = global;
+
 describe('Header component', () => {
-  const component = shallow(<Header />);
+  const wrapper = shallow(<Header />);
 
-  it('should exist', () => {
-    expect(component).toExist();
+  it('renders without exploding', () => {
+    expect(wrapper).toExist();
   });
 
-  it('should have one logo linking to the "Home" page', () => {
-    expect(component.find('Link[to="/"].header__branding').length).toEqual(1);
+  it('has one logo linking to the "Home" page', () => {
+    const expected = 1;
+    const actual = wrapper.find('Link[to="/"].header__branding').length;
+    expect(actual).toEqual(expected);
   });
 
-  it('should have a menu with 5 menu items', () => {
-    expect(component.find('.menu__link').length).toEqual(4);
+  it('has a menu with 4 menu items', () => {
+    const expected = 4;
+    const actual = wrapper.find('.menu__link').length;
+    expect(actual).toEqual(expected);
   });
 
-  it('should have one link to the "Users" page', () => {
-    expect(component.find('Link[to="/users"]').length).toEqual(1);
+  it('has one link to the "Users" page', () => {
+    const expected = 1;
+    const actual = wrapper.find('Link[to="/users"]').length;
+    expect(actual).toEqual(expected);
   });
 
-  it('should have one link to the "Home" page', () => {
-    expect(component.find('IndexLink[to="/"]').length).toEqual(1);
+  it('has one link to the "Home" page', () => {
+    const expected = 1;
+    const actual = wrapper.find('Link[to="/"]').length;
+    expect(actual).toEqual(expected);
   });
 
-  it('should have one link to "not found" page', () => {
-    expect(component.find('Link[to="/404"]').length).toEqual(1);
+  it('has one link to "Not found" page', () => {
+    const expected = 1;
+    const actual = wrapper.find('Link[to="/404"]').length;
+    expect(actual).toEqual(expected);
   });
 });
